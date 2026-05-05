dae15a66e71a41e067ce4d35b3177f467305de5e
Koutaro Mukai <mukai.k1011k@outlook.jp>
2026-05-05 12:40:17 +0900

    Update

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -1,9 +1,13 @@
 // @ts-check
-import { defineConfig } from '@vivliostyle/cli';
+import { defineConfig } from "@vivliostyle/cli";
 
 export default defineConfig({
   title: "vivliostyle-preview-in-container",
   author: "u1f992",
   language: "ja",
+  theme: "./theme",
   entry: ["public/manuscript.md"],
+  vfm: {
+    footnote: "gcpm",
+  },
 });
